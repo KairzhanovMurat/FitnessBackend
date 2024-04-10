568e83e08512a7cd9ad0927443d7f89715c4a02d
KairzhanovMurat <murat99010@gmail.com>
2024-04-11 03:10:00 +0500

    fixed bug with schedule time check

--- schedules/utils.py
+++ schedules/utils.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
 
 
+def _time_check(qs, start_time, end_time):
+    for schedule in qs:
+        if schedule.start_time < end_time and schedule.end_time > start_time:
+            raise serializers.ValidationError("User has conflicting schedules")
+
+
 def schedule_check(gym, start_time, end_time, day, model, instance=None, trainer=None, member=None):
     target_member = trainer if trainer is not None else member
     if target_member not in gym.members.all():
         raise serializers.ValidationError("User is not a member of the gym")
     conflicting_schedules = model.objects.filter(
         day=day,
-        start_time__lt=end_time,
-        end_time__gt=start_time,
     ).exclude(pk=instance.pk if instance else None)
     if trainer:
         conflicting_schedules = conflicting_schedules.filter(trainer=trainer)
     if member:
         conflicting_schedules = conflicting_schedules.filter(members=member)
-    if conflicting_schedules.exists():
-        raise serializers.ValidationError("User has conflicting schedules")
+    _time_check(conflicting_schedules, start_time, end_time)
